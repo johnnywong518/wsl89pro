19319ffaa8b215c897de07241772c34befcb2a29
johnny <johnnywong518@hotmail.com>
2019-06-23 20:14:44 +0800

    create test demo

---
+++ TestGit/src/com/yt/Test.java
@@ -0,0 +1,21 @@
+package com.yt;
+/**
+ * *
+ * <p>
+ * Title: Test.java
+ * </p>
+ * *
+ ** 
+ * @version:V 1.0.0
+ * @Description:
+ * @author: wsl
+ * @date: 2019年6月23日 下午8:12:26 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
